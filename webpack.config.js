4151bc14f97d9579b71a5233adedf37b176bf225
xerocross <adam.cross.inc@gmail.com>
2018-07-26 15:38:10 -0400

    more edits

---
+++ webpack.config.js
@@ -0,0 +1,12 @@
+const path = require('path');
+
+module.exports = {
+  entry: "./src",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "jsalgorithms-xerocross.js",
+    library: "JSAlgorithmsXerocross",
+    libraryTarget: "umd"
+  }
+  
+}
